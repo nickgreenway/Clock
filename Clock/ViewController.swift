a26b7fc0d71c566415c06f4d3a76b7732fd137e2
nickgreenway <nickgreenway@cpe-2606-a000-1408-73-ccc-ba56-599e-6348.dyn6.twc.com>
2016-11-15 15:47:11 -0500

    Initial Commit

---
+++ Clock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by Nick Greenway on 11/15/16.
+//  Copyright © 2016 Nick Greenway. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
